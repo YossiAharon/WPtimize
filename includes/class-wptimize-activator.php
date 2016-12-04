79b6bdedba687c986c9cb1b7853a14276db66455
Yossi Aharon <YossiAharon@users.noreply.github.com>
2016-12-04 11:42:03 +0200

    Add files via upload

---
+++ includes/class-wptimize-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       http://www.yossi.co.il/
+ * @since      1.0.0
+ *
+ * @package    Wptimize
+ * @subpackage Wptimize/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Wptimize
+ * @subpackage Wptimize/includes
+ * @author     Yossi Aharon <yossiaharon@yahoo.com>
+ */
+class Wptimize_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-wptimize-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       http://www.yossi.co.il/
+ * @since      1.0.0
+ *
+ * @package    Wptimize
+ * @subpackage Wptimize/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Wptimize
+ * @subpackage Wptimize/includes
+ * @author     Yossi Aharon <yossiaharon@yahoo.com>
+ */
+class Wptimize_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
