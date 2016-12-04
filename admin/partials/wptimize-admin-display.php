50aa8fe0ae3ba1fad7e2415a1f269c54f559e4a1
Yossi Aharon <YossiAharon@users.noreply.github.com>
2016-12-04 11:37:47 +0200

    Create wptimize-admin-display.php

---
+++ admin/partials/wptimize-admin-display.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Provide a admin area view for the plugin
+ *
+ * This file is used to markup the admin-facing aspects of the plugin.
+ *
+ * @link       http://www.yossi.co.il/
+ * @since      1.0.0
+ *
+ * @package    Wptimize
+ * @subpackage Wptimize/admin/partials
+ */
+?>
+
+<!-- This file should primarily consist of HTML with a little bit of PHP. -->
