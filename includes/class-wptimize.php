c93825748437a834fc63ac2cf207ecc2847160ea
Yossi Aharon <YossiAharon@users.noreply.github.com>
2016-12-04 16:49:34 +0200

    Add files via upload

---
+++ includes/class-wptimize.php
@@ -0,0 +1,278 @@
+<?php
+
+/**
+ * The file that defines the core plugin class
+ *
+ * A class definition that includes attributes and functions used across both the
+ * public-facing side of the site and the admin area.
+ *
+ * @link       http://www.yossi.co.il/
+ * @since      1.0.0
+ *
+ * @package    Wptimize
+ * @subpackage Wptimize/includes
+ */
+
+/**
+ * The core plugin class.
+ *
+ * This is used to define internationalization, admin-specific hooks, and
+ * public-facing site hooks.
+ *
+ * Also maintains the unique identifier of this plugin as well as the current
+ * version of the plugin.
+ *
+ * @since      1.0.0
+ * @package    Wptimize
+ * @subpackage Wptimize/includes
+ * @author     Yossi Aharon <yossiaharon@yahoo.com>
+ */
+class Wptimize {
+
+	/**
+	 * The loader that's responsible for maintaining and registering all hooks that power
+	 * the plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   protected
+	 * @var      Wptimize_Loader    $loader    Maintains and registers all hooks for the plugin.
+	 */
+	protected $loader;
+
+	/**
+	 * The unique identifier of this plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   protected
+	 * @var      string    $plugin_name    The string used to uniquely identify this plugin.
+	 */
+	protected $plugin_name;
+
+	/**
+	 * The current version of the plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   protected
+	 * @var      string    $version    The current version of the plugin.
+	 */
+	protected $version;
+	
+	/**
+	 * Unique identifier for your plugin options.
+	 *
+	 *
+	 *
+	 * @since    1.0.0
+	 *
+	 * @var      string
+	 */
+	private $options_slug;
+	/**
+	 * Default Settings Values.
+	 *
+	 * @since    1.0.0
+	 *
+	 * @var      string
+	 */
+	private $options_data;
+
+	/**
+	 * Define the core functionality of the plugin.
+	 *
+	 * Set the plugin name and the plugin version that can be used throughout the plugin.
+	 * Load the dependencies, define the locale, and set the hooks for the admin area and
+	 * the public-facing side of the site.
+	 *
+	 * @since    1.0.0
+	 */
+	public function __construct() {
+
+		$this->plugin_name = 'wptimize';
+		$this->version = '1.0.0';
+		$this->plugin_screen_hook_suffix = null;
+		$this->options_slug ='wptimize_options';
+		$this->options_data = array(
+			// Cleanup
+			'rsd_clean' => 0,
+			'wlwmanifest_clean' => 0,
+			'wp_generator_clean' => 0,
+			'shortlink_clean' => 0,
+		    'feed_links_clean' => 0,
+		    'feed_links_extra_clean' => 0,
+		    'rel_links_clean' => 0,
+		    'canonical_clean' => 0,
+			'emoji_clean' => 0,
+			'js2footer' => 0,
+			'query_string_clean' => 0,
+			'wp_api_clean' => 0,
+		);
+		$this->load_dependencies();
+		$this->set_locale();
+		$this->define_admin_hooks();
+		$this->define_public_hooks();
+
+	}
+
+	/**
+	 * Load the required dependencies for this plugin.
+	 *
+	 * Include the following files that make up the plugin:
+	 *
+	 * - Wptimize_Loader. Orchestrates the hooks of the plugin.
+	 * - Wptimize_i18n. Defines internationalization functionality.
+	 * - Wptimize_Admin. Defines all hooks for the admin area.
+	 * - Wptimize_Public. Defines all hooks for the public side of the site.
+	 *
+	 * Create an instance of the loader which will be used to register the hooks
+	 * with WordPress.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 */
+	private function load_dependencies() {
+
+		/**
+		 * The class responsible for orchestrating the actions and filters of the
+		 * core plugin.
+		 */
+		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-wptimize-loader.php';
+
+		/**
+		 * The class responsible for defining internationalization functionality
+		 * of the plugin.
+		 */
+		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-wptimize-i18n.php';
+
+		/**
+		 * The class responsible for defining all actions that occur in the admin area.
+		 */
+		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'admin/class-wptimize-admin.php';
+
+		/**
+		 * The class responsible for defining all actions that occur in the public-facing
+		 * side of the site.
+		 */
+		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'public/class-wptimize-public.php';
+
+		$this->loader = new Wptimize_Loader();
+
+	}
+
+	/**
+	 * Define the locale for this plugin for internationalization.
+	 *
+	 * Uses the Wptimize_i18n class in order to set the domain and to register the hook
+	 * with WordPress.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 */
+	private function set_locale() {
+
+		$plugin_i18n = new Wptimize_i18n();
+
+		$this->loader->add_action( 'plugins_loaded', $plugin_i18n, 'load_plugin_textdomain' );
+
+	}
+
+	/**
+	 * Register all of the hooks related to the admin area functionality
+	 * of the plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 */
+	private function define_admin_hooks() {
+
+		$plugin_admin = new Wptimize_Admin( $this->get_plugin_name(), $this->get_version(), $this->get_plugin_options_slug(), $this->get_plugin_options_data());
+
+		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_styles' );
+		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_scripts' );
+		$this->loader->add_action( 'admin_menu', $plugin_admin, 'add_options_page' );
+		$plugin_basename = plugin_basename( plugin_dir_path( __DIR__ ) . $this->plugin_name . '.php' );
+		$this->loader->add_filter( 'plugin_action_links_' . $plugin_basename, $plugin_admin, 'add_action_links' );
+		$this->loader->add_action('admin_init', $plugin_admin, 'options_update');
+
+	}
+
+	/**
+	 * Register all of the hooks related to the public-facing functionality
+	 * of the plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 */
+	private function define_public_hooks() {
+
+		$plugin_public = new Wptimize_Public( $this->get_plugin_name(), $this->get_version(), $this->get_plugin_options_slug(), $this->get_plugin_options_data() );
+
+		// $this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_styles' );
+		// $this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_scripts' );
+		// Cleanup - Actions and filters
+        //Actions
+        $this->loader->add_action( 'init', $plugin_public, 'wptimize_cleanup' );
+
+	}
+
+	/**
+	 * Run the loader to execute all of the hooks with WordPress.
+	 *
+	 * @since    1.0.0
+	 */
+	public function run() {
+		$this->loader->run();
+	}
+
+	/**
+	 * The name of the plugin used to uniquely identify it within the context of
+	 * WordPress and to define internationalization functionality.
+	 *
+	 * @since     1.0.0
+	 * @return    string    The name of the plugin.
+	 */
+	public function get_plugin_name() {
+		return $this->plugin_name;
+	}
+	
+	/**
+	 * Return the plugin options slug.
+	 *
+	 * @since    1.0.0
+	 *
+	 * @return    Plugin slug variable.
+	 */
+	public function get_plugin_options_slug() {
+		return $this->options_slug;
+	}
+	/**
+	 * Return the plugin options default data.
+	 *
+	 * @since    1.0.0
+	 *
+	 * @return    Plugin options variable.
+	 */
+	public function get_plugin_options_data() {
+		return $this->options_data;
+	}
+
+	/**
+	 * The reference to the class that orchestrates the hooks with the plugin.
+	 *
+	 * @since     1.0.0
+	 * @return    Wptimize_Loader    Orchestrates the hooks of the plugin.
+	 */
+	public function get_loader() {
+		return $this->loader;
+	}
+
+	/**
+	 * Retrieve the version number of the plugin.
+	 *
+	 * @since     1.0.0
+	 * @return    string    The version number of the plugin.
+	 */
+	public function get_version() {
+		return $this->version;
+	}
+
+}
