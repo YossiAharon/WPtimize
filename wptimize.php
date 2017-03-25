526ec2ef61710662d2f1deff6abc7bc19532a542
Yossi Aharon <YossiAharon@users.noreply.github.com>
2017-03-25 12:02:55 +0300

    Update admin header information

--- wptimize.php
+++ wptimize.php
@@ -14,6 +14,7 @@
  * Version:           1.0.0
  * Author:            Yossi Aharon
  * Author URI:        http://www.yossi.co.il/
+ * Donate link: https://www.paypal.me/YossiAharon
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       wptimize
